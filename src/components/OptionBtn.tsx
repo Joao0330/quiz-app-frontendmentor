adb637ca9293d58398b894e2266e10a015c8d00e
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2026-09-04 16:04:27 +0100

    Started working on the game logic

--- src/components/OptionBtn.tsx
+++ src/components/OptionBtn.tsx
@@ -1,17 +1,26 @@
 import correctIcon from '../assets/images/icon-correct.svg';
 import incorrectIcon from '../assets/images/icon-incorrect.svg';
 
-export const OptionBtn = () => {
+interface OptionBtnProps {
+	option: string;
+	optionIndex: number;
+	handleOptionSelect: (optionIndex: number) => void;
+	isSelected: boolean;
+}
+
+export const OptionBtn = ({ option, optionIndex, handleOptionSelect, isSelected }: OptionBtnProps) => {
+	const questionLetters = ['A', 'B', 'C', 'D'];
+
 	return (
 		<li>
-			<button type='button' className='optionBtn'>
+			<button type='button' className={`optionBtn${isSelected ? ' active' : ''} `} onClick={() => handleOptionSelect(optionIndex)}>
 				<div>
-					<span>A</span>
-					<strong>4.5:1</strong>
+					<span>{questionLetters[optionIndex]}</span>
+					<strong>{option}</strong>
 				</div>
 
-				<img src={correctIcon} alt='correct icon image' className='hidden' />
-				<img src={incorrectIcon} alt='correct icon image' className='hidden' />
+				<img src={correctIcon} alt='correct icon image' className={`hidden`} />
+				<img src={incorrectIcon} alt='incorrect icon image' className='hidden' />
 			</button>
 		</li>
 	);
--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -4,6 +4,7 @@ import { ThemeBtn } from '@/components/ThemeBtn';
 import errorIcon from '@/assets/images/icon-error.svg';
 import { quizzes, type QuizCategory } from '@/data';
 import quizData from '@/data/data.json';
+import { useState } from 'react';
 
 interface QuestionPageProps {
 	category: QuizCategory;
@@ -13,7 +14,37 @@ export const QuestionPage = ({ category }: QuestionPageProps) => {
 	const quiz = quizzes[category];
 	const quizQuestions = quizData.quizzes.find(q => q.title === category)?.questions;
 
-	console.log(quizQuestions);
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+	const [selectedOptionIndex, setSelectedOptionIndex] = useState<number | null>(null);
+	const [showError, setShowError] = useState<boolean>(false);
+
+	if (!quizQuestions || quizQuestions.length === 0) {
+		return <p className='text-red-500'>No questions found for this quiz.</p>;
+	}
+
+	const currentQuestion = quizQuestions[currentQuestionIndex];
+	const progressPercentage = ((currentQuestionIndex + 1) / quizQuestions.length) * 100;
+
+	const handleOptionSelect = (optionIndex: number) => {
+		setSelectedOptionIndex(currentIndex => (currentIndex === optionIndex ? null : optionIndex));
+	};
+
+	console.log(currentQuestion);
+	console.log(selectedOptionIndex);
+
+	const isAnswerCorrect = () => {
+		if (selectedOptionIndex === null) {
+			setShowError(true);
+			return;
+		} else {
+			setShowError(false);
+		}
+
+		if (currentQuestion.answer === currentQuestion.options[selectedOptionIndex]) {
+			console.log('Correct answer!');
+		}
+		// Finish this logic
+	};
 
 	return (
 		<section className='min-h-screen px-6 md:px-16 xl:px-0 xl:pr-35.75 xl:pl-35'>
@@ -32,29 +63,26 @@ export const QuestionPage = ({ category }: QuestionPageProps) => {
 			<div className='pt-8 pb-21.75 flex flex-col gap-10 md:pt-0 xl:flex-row xl:items-start xl:gap-32'>
 				<div className='xl:min-w-116.25'>
 					<p className='text-preset5-mobile text-grey-500 dark:text-blue-300 md:text-preset6'>
-						Question <span>6</span> of <span>10</span>
+						Question <span>{currentQuestionIndex + 1}</span> of <span>{quizQuestions.length}</span>
 					</p>
 
-					<h2 className='mt-4 mb-6 text-preset3-mobile text-blue-900 dark:text-white md:text-preset3 md:mt-6 md:mb-10 xl:mb-46'>
-						Which of these color contrast ratios defines the minimum WCAG 2.1 Level AA requirement for normal text?
-					</h2>
+					<h2 className='mt-4 mb-6 text-preset3-mobile text-blue-900 dark:text-white md:text-preset3 md:mt-6 md:mb-10 xl:mb-46'>{currentQuestion.question}</h2>
 
-					<div className='p-1 rounded-full bg-white dark:bg-blue-850'>
-						<div className='h-2 rounded-full bg-purple-600 w-[60%]'></div>
+					<div className='w-full p-1 rounded-full bg-white dark:bg-blue-850'>
+						<div className='h-2 rounded-full bg-purple-600' style={{ width: `${progressPercentage}%` }}></div>
 					</div>
 				</div>
 
 				<div className='xl:min-w-141'>
 					<ol className='flex flex-col gap-4 md:gap-6'>
-						<OptionBtn />
-						<OptionBtn />
-						<OptionBtn />
-						<OptionBtn />
+						{currentQuestion.options.map((option, optionIndex) => (
+							<OptionBtn key={option} option={option} optionIndex={optionIndex} handleOptionSelect={handleOptionSelect} isSelected={selectedOptionIndex === optionIndex} />
+						))}
 					</ol>
 
-					<SubmitBtn />
+					<SubmitBtn isAnswerCorrect={isAnswerCorrect} />
 
-					<div className='hidden items-center justify-center gap-2 mt-4 md:mt-8'>
+					<div className={`${showError ? 'flex' : 'hidden'} items-center justify-center gap-2 mt-4 md:mt-8`}>
 						<img src={errorIcon} alt='icon image representing an error' className='h-8 w-8' />
 						<p className='text-red-500 text-preset4-mobile dark:text-white md:text-preset5-regular'>Please select an answer</p>
 					</div>
