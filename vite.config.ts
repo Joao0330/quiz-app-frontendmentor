8d75d9f221722f33062e132ef5590de233daf5d7
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2026-09-08 11:35:09 +0100

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/quiz-app-frontendmentor/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
